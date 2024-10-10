04c03447ba5bf4e63545e4433d0f83f48e5f946c
poseidon-1906 <youmbibryan@mail.com>
2024-10-10 19:53:00 +0100

    first commit

---
+++ src/main/java/dev/poseidon/movies/MovieService.java
@@ -0,0 +1,29 @@
+package dev.poseidon.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+
+    public List<Movie> allMovies(){
+
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+}
